a13891fa730e5f871c6f7d5c9c3ced0eeb2234b7
Nithin Nathanael <nithinbolapati7@gmail.com>
2026-04-29 16:05:25 +0530

    code-1

--- test_files/code_1.py
+++ test_files/code_1.py
@@ -4,6 +4,7 @@ users = {
     "admin": "5f4dcc3b5aa765d61d8327deb882cf99"  # md5("password")
 }
 
+
 def login(username, password):
     hashed = hashlib.md5(password.encode()).hexdigest()
 
@@ -11,6 +12,7 @@ def login(username, password):
         if users[username] == hashed:
             return True
     return False
+
 
 def get_user_data(username):
     return users[username]
