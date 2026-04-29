cbbf903e71184e4d8b22edad36051598a3e54250
Nithin Nathanael <nithinbolapati7@gmail.com>
2026-04-29 16:00:57 +0530

    Small change on adding new code to random_code.py

--- test_files/random_code.py
+++ test_files/random_code.py
@@ -1,3 +1,16 @@
-def calculate_average(numbers):
-    total = sum(numbers)
-    return total / len(numbers)
+import hashlib
+
+users = {
+    "admin": "5f4dcc3b5aa765d61d8327deb882cf99"  # md5("password")
+}
+
+def login(username, password):
+    hashed = hashlib.md5(password.encode()).hexdigest()
+
+    if username in users:
+        if users[username] == hashed:
+            return True
+    return False
+
+def get_user_data(username):
+    return users[username]
